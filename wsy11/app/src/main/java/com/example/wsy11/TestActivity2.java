f5b6222ecdc1c086420a35916f369bd817ddd8e4
wangshiyan54 <1983195822@qq.com>
2020-12-19 12:30:30 +0800

    第十一次作业

---
+++ wsy11/app/src/main/java/com/example/wsy11/TestActivity2.java
@@ -0,0 +1,15 @@
+package com.example.wsy11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class TestActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test2);
+    }
+}
